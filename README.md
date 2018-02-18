8bb943ee16775dd53c625c97a8f50d378af269bf
Samantha Neilen <33734383+SamanthaNeilen@users.noreply.github.com>
2018-02-18 10:57:31 +0100

    Finished windows tests and appended readme file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
     This project was created with Visual Studio 2017 Community Edition.
 </p>
 <p>
-    This project will be used to demonstrate Selenium and Wininum UI tests.
+    This project will be used to demonstrate <a href="http://www.seleniumhq.org/" target=_blank">Selenium</a> and <a href="https://github.com/2gis/Winium.Desktop" target="_blank">Wininum.Desktop</a> UI tests.
 </p> 
 <h3>How to use</h3>
 <p>
@@ -13,10 +13,24 @@
 	To run the WebTest_Selenium_TestForm test, first run the CodedUITestApplication.Web without debugging. 
 	Next start the test from the Test Explorer window.
 </p>
+<p>
+	To run the Test_Windows_Calculator test, make sure that path to calc.exe is vlid for your machine.
+	Next start the test from the Test Explorer window. This test was written for the calculator on a Windows 2012 server with english locale and may not work on other locales or with the newer implementation of windows calculator for Windows 10.
+</p>
+<p>
+	To run the Test_WPF_Addition test, make sure to build the CodedUITestApplication.WPF in debug mode. 
+	Next run the test from the Test Explorer window.
+</p>
 <h3>Extra note on running Selenium tests</h3>
 <p>
 This project uses the files in the Assets folder of the test project. All files in the folder have a build action and get copied to the bin folder where they are referenced from the code or web.config.
 When using older MS test frameworks that run in the seperate TestResults/Out folder, you will need to use a TestSettings file to get the Assets folder deployed to that directory.
+</p>
+
+<h3>Discovering the control Id values for your windows applications</h3>
+<p>
+If you do not know the Id of a windows control you can use the inspect.exe tool (<a href="https://msdn.microsoft.com/en-us/library/windows/desktop/dd318521(v=vs.85).aspx" target="_blank">https://msdn.microsoft.com/en-us/library/windows/desktop/dd318521(v=vs.85).aspx</a>).
+This tool can be installed via Visual Studio by installing the Windows 10 SDK (10.0.16299.0) for UWP: C#, VB, JS. (version number may vary). The tool will be installed in the default directory: C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x86\inspect.exe.
 </p>
 		
 
--- WebDriverTestApplication.Tests/WindowsTests.cs
+++ WebDriverTestApplication.Tests/WindowsTests.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Remote;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace WebDriverTestApplication.Tests
@@ -11,6 +12,12 @@ namespace WebDriverTestApplication.Tests
     [TestClass]
     public class WindowsTests
     {
+        // Automation Id's are found using the inspect.exe tool (https://msdn.microsoft.com/en-us/library/windows/desktop/dd318521(v=vs.85).aspx)
+        private const string BUTTON_5_AUTOMATION_ID = "135";
+        private const string BUTTON_ADD_AUTOMATION_ID = "93";
+        private const string BUTTON_EQUALS_AUTOMATION_ID = "121";
+        private const string RESULT_AUTOMATION_ID = "150";
+
         [TestMethod]
         public void Test_Windows_Calculator()
         {
@@ -22,30 +29,59 @@ namespace WebDriverTestApplication.Tests
             dc.SetCapability("app", @"C:/windows/system32/calc.exe");
             var driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc);
 
-            var numpad = driver.FindElementById("NumberPad");
-            var operations = driver.FindElementById("StandardOperators");
-
-            var button5 = numpad.FindElement(By.Id("num5Button"));
+            var window = driver.FindElementByClassName("CalcFrame");
+            
+            var button5 = window.FindElement(By.Id(BUTTON_5_AUTOMATION_ID));
             button5.Click();
 
-            var buttonPlus = operations.FindElement(By.Id("plusButton"));
+            var buttonPlus = window.FindElement(By.Id(BUTTON_ADD_AUTOMATION_ID));
             buttonPlus.Click();
-            
+
             button5.Click();
-            
-            var buttonEquals = operations.FindElement(By.Id("equalButton"));
+
+            var buttonEquals = window.FindElement(By.Id(BUTTON_EQUALS_AUTOMATION_ID));
             buttonEquals.Click();
-
-            var resultArea = driver.FindElementById("CalculatorResults");
-           // Assert.AreEqual("10", resultArea.Text);
-
-            var resultText = resultArea.FindElement(By.Id("textContainer"));
-            Assert.AreEqual("10", resultText.Text);
+            
+            var resultTextElement = window.FindElement(By.Id(RESULT_AUTOMATION_ID));
+            Assert.AreEqual("10", resultTextElement.GetAttribute("Name"));
 
             driver.Close();
             driver.Dispose();
-            winiumdriverProcess.Close();
+            winiumdriverProcess.CloseMainWindow();
+            winiumdriverProcess.Dispose();
+        }
+
+
+        [TestMethod]
+        public void Test_WPF_Addition()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Assets\\Winium.Desktop.Driver.exe";
+            var winiumdriverProcess = Process.Start(startInfo);
+
+            var dc = new DesiredCapabilities();
+            dc.SetCapability("app", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\..\..\..\WebDriverTestApplication.WPF\bin\Debug\WebDriverTestApplication.WPF.exe");
+            var driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc);
+
+            var window = driver.FindElementById("Wpf test app");
+
+            var amount1Edit = window.FindElement(By.Id("txtFirstAmount"));
+            amount1Edit.SendKeys("5");
+
+            var amount2Edit = window.FindElement(By.Id("txtSecondAmount"));
+            amount2Edit.SendKeys("10");
+
+            var buttonAdd = window.FindElement(By.Id("btnAdd"));
+            buttonAdd.Click();
+
+            var resultLabel = window.FindElement(By.Id("lblResult"));
+            Assert.AreEqual("15", resultLabel.GetAttribute("Name"));
+
+            driver.Close();
+            driver.Dispose();
+            winiumdriverProcess.CloseMainWindow();
             winiumdriverProcess.Dispose();
         }
     }
 }
+
